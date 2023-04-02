6111f8e4e336aceff682df83e9a344a18d8ec77a
Joosep Parts <joosep.parts@gmail.com>
2023-04-02 06:38:32 +0300

    literature review

---
+++ literature-review/update_date.py
@@ -0,0 +1,23 @@
+import os
+import datetime
+
+today = datetime.datetime.today().strftime('%d.%m.%Y')
+
+project_dir = './'
+
+for root, dirs, files in os.walk(project_dir):
+    for file in files:
+        if file.endswith('.tex'):
+            file_path = os.path.join(root, file)
+            with open(file_path, 'r') as f:
+                content = f.readlines()
+
+            if content[0].startswith('%! Author') and content[1].startswith('%! Date'):
+                content[0] = '%! Author = partsjoo\n'
+                content[1] = f'%! Date = {today}\n'
+            else:
+                content.insert(0, f'%! Date = {today}\n')
+                content.insert(0, '%! Author = partsjoo\n')
+
+            with open(file_path, 'w') as f:
+                f.writelines(content)
